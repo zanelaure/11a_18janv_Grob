97f4f876c3b213e6f6a75aa1037dce4b59acf861
unknown <zane.laure@grobinasskola.lv>
2024-01-18 15:20:12 +0200

    Init commit

--- 1uzd.py
+++ 1uzd.py
@@ -1,2 +1,12 @@
 #Izveidot Python programmu, kas nolasītu un izdrukātu visu teksta faila saturu (txt formātā). 
 
+def read_file(file_name):
+    try:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print("Datne nav atrasta")
+
+file_name = input("Ievadiet teksta faila nosaukumu (ar .txt paplašinājumu): ")
+read_file(file_name)
--- 5uzd.py
+++ 5uzd.py
@@ -2,3 +2,7 @@
 #Ievadīto vārdu pēc tam ierakstīt teksta failā (piemēram, "lietotajs.txt").
 #Programmai jābūt spējīgai apstrādāt kļūdas, piemēram, faila nepieejamību vai rakstīšanas problēmas.
 #Pēc ierakstīšanas izvadīt paziņojumu par veiksmīgu darbību vai kļūdu gadījumā attiecīgu kļūdas ziņojumu.
+import json
+file_name = input
+with  open('lietotajs.txt', 'w', encoding='utf-8') as bum:
+    json.dump(...)
